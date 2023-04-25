0384fb1974798aa7b86de1f95a9a5d472377990d
Anderson Amantino <88107035+anderson-mntn@users.noreply.github.com>
2023-04-25 12:06:43 -0300

    add: experesIn and jwt.verify()

--- tokenPlayground.js
+++ tokenPlayground.js
@@ -16,7 +16,22 @@ const secret = "9g8t4"
 //const token = jwt.sign({user : user.id, email: user.email}, secret, {expiresIn: 50})
 //console.log(token);
 
-const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiNDA1MCIsImVtYWlsIjoiam9hb2FsbWVpZGFAZ21haWwuY29tIiwiaWF0IjoxNjgyNDM0NTc2LCJleHAiOjE2ODI0MzQ2MjZ9.3Vgi9MOz5Abyle1kF02E3za60zC_n381yVFPxaFpjgE"
+function createToken(){
+    const token = jwt.sign({user : user.id, email: user.email}, secret, {expiresIn: 50})
+    console.log(token);
+}
 
-const validData = jwt.verify(token, secret)
-console.log(validData);
+
+function verifyToken(token){
+    try {
+        const validData = jwt.verify(token, secret)
+        console.log(validData);
+    } catch (error) {
+        console.log(error);        
+    }
+}
+
+//createToken();
+
+//Deve ser passado uma string - jwt.verify
+verifyToken('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiNDA1MCIsImVtYWlsIjoiam9hb2FsbWVpZGFAZ21haWwuY29tIiwiaWF0IjoxNjgyNDM1MTA5LCJleHAiOjE2ODI0MzUxNTl9.H3ikHNgJDesYGzgUiTMy9NplXxyXFYghwiODG08tuxk')
