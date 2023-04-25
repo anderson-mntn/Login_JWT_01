2a53392702d79f259da7fe20373d7eb0f995d6a2
Anderson Amantino <88107035+anderson-mntn@users.noreply.github.com>
2023-04-25 17:24:56 -0300

    add comments

--- routes/adminRouter.js
+++ routes/adminRouter.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const auth = require('../controllers/authController')
+
+const auth = require('../controllers/authController') //verificador de autenticação - retorna a verificação de um token
 
 router.get('/', auth, (req, res)=>{
 
     // req.user agora corresponde ao usuário verificado e podemos usar para verificar se é admin
-
+    // req.user.admin será true ou false, dependendo de como está no db
     if(req.user.admin){
         res.send("Dados do admin")
     } else {
@@ -14,6 +15,7 @@ router.get('/', auth, (req, res)=>{
 
 })
 
+// rota /admin/free 
 router.get('/free', auth,(req, res)=>{
 
     res.send('Este dados só deve ser visto quando se está logado.')
