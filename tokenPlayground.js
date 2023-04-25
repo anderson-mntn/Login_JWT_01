194866da047d57cbca452fb70f9850fb8d95ac94
Anderson Amantino <88107035+anderson-mntn@users.noreply.github.com>
2023-04-25 11:57:26 -0300

    basic token validation

--- tokenPlayground.js
+++ tokenPlayground.js
@@ -13,6 +13,10 @@ const user = {
 const secret = "9g8t4"
 
 // passar payload e secret
-const token = jwt.sign({user : user.id, email: user.email}, secret)
+//const token = jwt.sign({user : user.id, email: user.email}, secret, {expiresIn: 50})
+//console.log(token);
 
-console.log(token);
+const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiNDA1MCIsImVtYWlsIjoiam9hb2FsbWVpZGFAZ21haWwuY29tIiwiaWF0IjoxNjgyNDM0NTc2LCJleHAiOjE2ODI0MzQ2MjZ9.3Vgi9MOz5Abyle1kF02E3za60zC_n381yVFPxaFpjgE"
+
+const validData = jwt.verify(token, secret)
+console.log(validData);
